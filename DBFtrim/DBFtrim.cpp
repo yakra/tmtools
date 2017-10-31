af5d5472fe6173a9452a6aa1f0e5254498f87d19
Eric Bryant <ericdbryant@yahoo.com>
2017-10-31 15:38:47 -0400

    20171031d, move whitespace trim to field::GetMax
    only applies to type C fields

--- DBFtrim/DBFtrim.cpp
+++ DBFtrim/DBFtrim.cpp
@@ -86,6 +86,7 @@ class DBF
 void field::GetMax(DBF& tDBF, unsigned int fNum, char* value)
 {	switch (type)
 	{   case 'C':
+		while (value[strlen(value)-1] == ' ') value[strlen(value)-1] = 0; // trim whitespace
 		if (!MaxVal)
 		{	tDBF.fArr[fNum].len = 0;
 			MaxVal = new char[1]; MaxVal[0] = '\0';
@@ -129,7 +130,6 @@ int main(int argc, char *argv[])
 		for (unsigned int fNum = 0; fNum < oDBF.NumFields; fNum++)
 		{	char *value = new char[oDBF.fArr[fNum].len+1]; value[oDBF.fArr[fNum].len] = 0;
 			inDBF.read(value, oDBF.fArr[fNum].len);					// read in value from file
-			while (value[strlen(value)-1] == ' ') value[strlen(value)-1] = 0;	// trim whitespace
 			oDBF.fArr[fNum].GetMax(tDBF, fNum, value);
 		}
 	}//*/
