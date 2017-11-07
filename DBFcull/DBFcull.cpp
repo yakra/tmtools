a72bc91bc03f96c831c66a9ff036a0c2d80e6d9b
Eric Bryant <ericdbryant@yahoo.com>
2017-11-07 12:47:25 -0500

    20171107a
    use classes developed for DBFtrim
    DBF & field -> header files
    earliest version committed to GitHub
    earlier versions saved locally

---
+++ DBFcull/DBFcull.cpp
@@ -0,0 +1,71 @@
+using namespace std;
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
+class DBF;
+#include "../lib/field.h"
+#include "../lib/dbf.cpp"
+
+bool match(int argc, char *argv[], DBF& DBFh, bool *keep, unsigned int index)
+{	for (int a = 3; a < argc; a++)
+	  if (!strcmp(DBFh.fArr[index].name, argv[a]))
+	  {	DBFh.HeaLen -= 0x20;
+		DBFh.RecLen -= DBFh.fArr[index].len;
+		keep[index] = 0;
+		return 0;
+	  }
+	keep[index] = 1;
+	return 1;
+}
+
+int main(int argc, char *argv[])
+{	cout << endl;
+	if (argc < 4)	{ cout << "usage: ./DBFcull InputFile OutputFile Field1 (Field2) (...)\n\n"; return 0; }
+	if (!strcmp(argv[1], argv[2]))
+	{	cout << "FATAL ERROR: input and output file paths match!\n";
+		cout << "If you wanna be clever, try /home/dave/foo.dbf & /home/dave/./foo.dbf or something\n\n";
+		return 0;
+	}
+
+	bool OK;
+	DBF DBFh(argv[1], OK);	if (!OK) return 0;
+	bool *keep = new bool[DBFh.NumFields]; // whether to cull or keep each field
+
+	//field info display
+	cout << "FieldName\tType\tLength\tKeep?\n";
+	for (unsigned int i = 0; i < DBFh.NumFields; i++)
+	{	cout << DBFh.fArr[i].name;	if (strlen(DBFh.fArr[i].name) < 8) cout << '\t'; // tab stop
+		cout << '\t' << DBFh.fArr[i].type;
+		cout << '\t' << int(DBFh.fArr[i].len);
+		if (match(argc, argv, DBFh, keep, i))
+			cout << "\tkeep\n";
+		else	cout << "\tcull\n";
+	}
+	cout << "New Header Length:\t0x" << hex << DBFh.HeaLen << '\t' << dec << DBFh.HeaLen << endl;
+	cout << "New Record Length:\t0x" << hex << DBFh.RecLen << '\t' << dec << DBFh.RecLen << endl;
+
+	// write output file
+	ifstream inDBF(argv[1], ios::in);
+	inDBF.seekg(0x20);
+	ofstream outDBF(argv[2]);
+	// write header
+	outDBF.write((char*)&DBFh, 32);
+	for (unsigned int n = 0; n < DBFh.NumFields; n++)				// field descriptor array
+		if (keep[n]) for (char c = 0; c < 0x20; c++) outDBF.put(inDBF.get());	// keep & copy field descriptor
+		else inDBF.seekg(0x20, ios::cur);					// cull & skip field descriptor
+	outDBF.put(inDBF.get());							// 0Dh stored as the field terminator
+	// the actual records
+	for (unsigned int r = 0; r < DBFh.NumRec; r++)
+	{	outDBF.put(inDBF.get()); // either ' ' or '*'
+		for (unsigned int n = 0; n < DBFh.NumFields; n++)
+			if (keep[n])
+				for (unsigned char c = 0; c < DBFh.fArr[n].len; c++) outDBF.put(inDBF.get());	// keep & copy field
+			else	inDBF.seekg(DBFh.fArr[n].len, ios::cur);						// cull & skip field
+	}
+	// EOF marker
+	if (!DBFh.borderline) outDBF.put(char(0x1A));
+
+	// one more newline for the road
+	cout << endl;
+}
