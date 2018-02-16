8bee0e1e032906c3cf82bab36d71ac1d92e13e0e
Eric Bryant <ericdbryant@yahoo.com>
2018-02-16 03:31:14 -0500

    20180216, use std::list

--- DBFmine/DBFmine.cpp
+++ DBFmine/DBFmine.cpp
@@ -1,3 +1,4 @@
+#include <list>
 #include "../lib/dbf.cpp"	// includes cstring, fstream, iostream
 using namespace std;
 
@@ -5,35 +6,12 @@ inline void ProgBar(unsigned int numerator, unsigned int denominator)
 {	cout << numerator << '/' << denominator << char(0x0D);
 }
 
-class entry
-{	public:
-	char *value;
-	entry *prev, *next;
-
-	//init alpha & omega
-	entry(unsigned char FieldLength, char letter)
-	{	value = new char[FieldLength+1];
-		for (unsigned char index = 0; index < FieldLength; index++) value[index] = letter;
-		value[FieldLength] = 0;
-	}
-	//insertions
-	entry(entry *cursor, char *KFieldVal)
-	{	prev = cursor->prev;
-		prev->next = this;
-		next = cursor;
-		next->prev = this;
-		value = KFieldVal;
-	}
-};
-
 void mine(DBF& rDBF)
 {	cout << "Mining...\n";
 
-	//init alpha & omega
 	char *KFieldVal;
-	entry alpha(rDBF.KeyLen, 0), omega(rDBF.KeyLen, 255), *cursor;
-	alpha.prev = 0; alpha.next = &omega;
-	omega.prev = &alpha; omega.next = 0;
+	list<char*> ValList(1, (char*)"\0");
+	list<char*>::iterator it;
 
 	ofstream file("output.txt", ios::app);
 	ifstream fDBF(rDBF.name);
@@ -47,15 +25,17 @@ void mine(DBF& rDBF)
 		fDBF.read(KFieldVal, rDBF.KeyLen);
 		fDBF.seekg(rDBF.RecLen-rDBF.KeyLen, ios::cur);
 		// scan thru asciibetical list
-		for (cursor = &alpha; strcmp(KFieldVal, cursor->value) > 0; cursor = cursor->next);
-		if (strcmp(KFieldVal, cursor->value) < 0)
-			new entry(cursor, KFieldVal);	// insert new entry into list before cursor
-		else delete KFieldVal;			// strcmp == 0; value already on list
+		for (it = ValList.begin(); it != ValList.end(); it++) if (strcmp(KFieldVal, *it) <= 0) break;
+		if (it == ValList.end()) ValList.insert(it, KFieldVal);
+		else	if (strcmp(KFieldVal, *it) < 0)
+				ValList.insert(it, KFieldVal);
+			else delete KFieldVal;			// strcmp == 0; value already on list
 
 		ProgBar(RecNum+1, rDBF.NumRec); //zeroth vs first
 	}
 	fDBF.close();
-	for (cursor = alpha.next; cursor->next; cursor = cursor->next) file << cursor->value << endl;
+	it = ValList.begin();
+	for (it++; it != ValList.end(); it++) file << *it << endl;
 	cout << endl;
 }
 
