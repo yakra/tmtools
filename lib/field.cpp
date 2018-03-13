45ea08938edc10b861da81b65bb27b7a66ec0886
Eric Bryant <ericdbryant@yahoo.com>
2018-03-12 22:43:53 -0400

    field.cpp: trim whitespace from both left & right

--- lib/field.cpp
+++ lib/field.cpp
@@ -1,42 +1,39 @@
 #include "dbf.cpp"
 
-void field::GetMax(DBF& tDBF, unsigned int fNum, char* fVal)
+void fTrim(char* fVal, unsigned char &vlen)
 {	unsigned char pad;
-	unsigned char vlen = strlen(fVal);
+	// trim whitespace RIGHT
+	for (int i = vlen-1; fVal[i] <= ' ' && fVal[i] > 0 && i >= 0; i--) { fVal[i] = 0; vlen--; }
+	// trim whitespace LEFT
+	for (pad = 0; fVal[pad] <= ' ' && pad < vlen; pad++);
+	vlen -= pad;
+	for (unsigned char i = 0; i <= vlen; i++) fVal[i] = fVal[pad+i];
+}
+
+void field::GetMax(DBF& tDBF, unsigned int fNum, char* fVal)
+{	unsigned char vlen = strlen(fVal);
 	unsigned char IntD = 0;
-	switch (type)
-	{   case 'C':
-		// trim whitespace RIGHT
-		for (int i = vlen-1; fVal[i] <= ' ' && fVal[i] > 0 && i >= 0; i--) { fVal[i] = 0; vlen--; }
-		break;
-
-	    case 'F':
-	    case 'N':
-		// trim whitespace LEFT
-		for (pad = 0; fVal[pad] <= ' ' && pad < vlen; pad++);
-		vlen -= pad;
-		for (unsigned char i = 0; i <= vlen; i++) fVal[i] = fVal[pad+i];
-
-		// trim extraneous trailing zeros
-		if (strchr(fVal, '.') && !strchr(fVal, 'E') && !strchr(fVal, 'e'))
-		{	pad = 0;
-			IntD = strchr(fVal, '.') - fVal;
-			if (IntD > MaxIntD) MaxIntD = IntD;
-			for (unsigned char i = vlen-1; fVal[i] == '0'; i--) pad++;
-			if (pad < MinEx0)
-			{	MinEx0 = pad;
-				if (MinEx0 >= DecCount)
-				{	MinEx0++; // decimal point itself is extraneous
-					tDBF.fArr[fNum].DecCount = 0; // prevent wraparound when E.G. "0.0000000000" has a DecCount of 0
-				}
-				else	tDBF.fArr[fNum].DecCount = DecCount-MinEx0;
+	fTrim(fVal, vlen);
+	// trim extraneous trailing zeros
+	if (type == 'F' || type == 'N')
+	  if (strchr(fVal, '.') && !strchr(fVal, 'E') && !strchr(fVal, 'e'))
+	  {	unsigned char pad = 0;
+		IntD = strchr(fVal, '.') - fVal;
+		if (IntD > MaxIntD) MaxIntD = IntD;
+		for (unsigned char i = vlen-1; fVal[i] == '0'; i--) pad++;
+		if (pad < MinEx0)
+		{	MinEx0 = pad;
+			if (MinEx0 >= DecCount)
+			{	MinEx0++; // decimal point itself is extraneous
+				tDBF.fArr[fNum].DecCount = 0; // prevent wraparound when E.G. "0.0000000000" has a DecCount of 0
 			}
+			else	tDBF.fArr[fNum].DecCount = DecCount-MinEx0;
 		}
-		else if (vlen)
+	  }
+	  else	if (vlen)
 		{	MinEx0 = 0;
 			tDBF.fArr[fNum].DecCount = DecCount;
 		}
-	}
 	// compare
 	if (vlen-IntD+MaxIntD > tDBF.fArr[fNum].len+MinEx0)
 	{	tDBF.fArr[fNum].len = vlen-IntD-MinEx0+MaxIntD;
