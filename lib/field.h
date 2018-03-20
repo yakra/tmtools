8ba624ec24e17f402892d0a1720e7fa61a3687a9
Eric Bryant <ericdbryant@yahoo.com>
2018-03-20 02:12:02 -0400

    field.h: MinEx0 -> x0term; add subtype prototype

--- lib/field.h
+++ lib/field.h
@@ -7,9 +7,10 @@ class field
 	char reserved1[4];
 	unsigned char len;
 	unsigned char DecCount;
-	unsigned char MinEx0;	// Minimum extraneous 0s. Not part of DBF spec; stored in reserved bytes for use by DBFtrim.
-	unsigned char MaxIntD;	// Maximum Integer digits. Not part of DBF spec; stored in reserved bytes for use by DBFtrim.
+	unsigned char x0term;	// Not part of DBF spec; stored in reserved bytes for use by DBFtrim: New terminus for MaxVals with extraneous '0's. 0 = display whole string
+	unsigned char MaxIntD;	// Not part of DBF spec; stored in reserved bytes for use by DBFtrim: Maximum Integer digits.
 	char reserved2[12];
 	// No more variables! Must be able to write/read a whole array to/from disk.
 	void GetMax(DBF&, unsigned int, char*);
+	unsigned char subtype(char*);
 };
