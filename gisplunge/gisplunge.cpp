dd06e658ceb8e09e2688b7ad0fcb41c8bba4eae6
Eric Bryant <ericdbryant@yahoo.com>
2018-04-30 16:44:00 -0400

    20180430, CSVline size bugfix
    Directly setting last character to 0 did not reduce CSVline.size()
    In effect, there were null 0s *before* the null 0 terminator!
    This caused wacky hijinks when attempting to append ".wpt" to Roots extracted from a CSV line with no AltRouteNames column.

--- gisplunge/gisplunge.cpp
+++ gisplunge/gisplunge.cpp
@@ -279,7 +279,7 @@ bool CSVmode(envV &env, DBF &dbf)
 	{	string System, Region, Route, Banner, Abbrev, City, Root, AltRouteNames;
 		string CSVline; // read individual line
 		for (char charlie = 0; charlie != '\n' && CSV.tellg() < EoF; CSVline.push_back(charlie)) CSV.get(charlie);
-		if (CSVline[CSVline.size()-1] == '\n') CSVline[CSVline.size()-1] = 0; //account for missing terminal '\n'
+		if (CSVline.back() == '\n') CSVline.erase(CSVline.end()-1); //account for missing terminal '\n'
 		// parse CSV line
 		unsigned int i = 0;
 		while (CSVline[i] != ';') { System.push_back(CSVline[i]); i++; } i++;
