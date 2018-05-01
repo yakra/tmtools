4ed5a6c2a09e9100b4cbd19e9565506a79eab68c
Eric Bryant <ericdbryant@yahoo.com>
2018-04-30 22:38:15 -0400

    20180430a, "canvas2018": now using tmtools classes

--- canvas/canvas.cpp
+++ canvas/canvas.cpp
@@ -1,297 +1,6 @@
 using namespace std;
-#include <fstream>
-#include <iostream>
-#include <cstring>
 #include <cmath>
-#include <string>
-
-class waypoint
-{	public:
-	unsigned int NumLabels;
-	char **label;
-	double OrigLat, OrigLon, OffLat, OffLon, OnLat, OnLon, OffDist, OnDist;
-	waypoint *prev, *next;
-	
-	waypoint(waypoint *ref)
-	{	prev = ref;
-		NumLabels = 0;
-		label = 0;
-		next = 0;
-	}
-
-	~waypoint()
-	{	if (prev) prev->next = next;
-		if (next) next->prev = prev;
-		for (unsigned int l = 0; l < NumLabels; l++)
-			delete[] label[l];
-		if (NumLabels) delete[] label;
-	}
-
-	void InitLabels()
-	{	if (!NumLabels) return;
-		label = new char*[NumLabels];
-		for (unsigned int l = 0; l < NumLabels; l++)
-			label[l] = new char[256];
-	}
-};
-
-class highway
-{	public:
-	char System[256], Region[256], Route[256], Banner[256], Abbrev[256], City[256], Root[256];
-	unsigned int NumAltRtes; char **AltRouteNames;
-	char Name[768], KeyValue[256];
-	bool KeyFound;
-	unsigned int NumPts;
-	waypoint *pt1;
-	highway *next;
-
-	highway
-	(	char SysIn[256],
-		char RegIn[256],
-		char RteIn[256],
-		char BanIn[256],
-		char AbbIn[256],
-		char CityIn[256],
-		char RootIn[256],
-		unsigned int NAR,
-		char **ARN,
-		char KeyIn[256]
-	)
-
-	{	NumPts = 0;
-		strcpy(System, SysIn);
-		strcpy(Region, RegIn);
-		strcpy(Route, RteIn);
-		strcpy(Banner, BanIn);
-		strcpy(Abbrev, AbbIn);
-		strcpy(City, CityIn);
-		strcpy(Root, RootIn);
-		strcpy(KeyValue, KeyIn);
-		NumAltRtes = NAR;
-		AltRouteNames = ARN;
-		KeyFound = 0;
-		next = 0;
-		strcpy(Name, Route); strcat(Name, Banner); strcat(Name, Abbrev);
-	}
-
-	~highway()
-	{	if (pt1)
-		{	while (pt1->next) delete pt1->next;
-			delete pt1;
-		}
-	}
-
-	unsigned int LblInd(char *lbl) // returning a value >= NumLabels = failure.
-	{	unsigned int p = 0;
-		for (waypoint *point = pt1; p < NumPts; p++)
-		{	for (unsigned int l = 0; l < point->NumLabels; l++)
-				if (!strcmp(point->label[l], lbl))
-				{	if (l) cout << Region << ' ' << Name << ' ' << point->label[l] << ": deprecated in favor of " << point->label[0] << endl;
-					return p;
-				}
-			point = point->next;
-		}
-		cout << Region << ' ' << Name << ' ' << lbl << ": point label not recognized\n";
-		return p;
-	}
-
-	void CSVline()
-	{	cout << System << ';' << Region << ';' << Route << ';' << Banner << ';' << Abbrev << ';' << City << ';' << Root << ';';
-		if (NumAltRtes)
-		{	cout << AltRouteNames[0];
-			for (unsigned short i = 1; i < NumAltRtes; i++) cout << ',' << AltRouteNames[i];
-		}
-		cout << endl;
-	}
-
-	bool NameMatch(const char *string)
-	{	if (!strcmp(string, Name)) return 1;
-		for (unsigned short i = 0; i < NumAltRtes; i++)
-			if (!strcmp(string, AltRouteNames[i])) return 1;
-		return 0;
-	}
-}; //end highway class
-
-// replace this idiotic function with calls to strstr, or something
-bool parseURL(char *URL, double *wptLat, double *wptLon)
-{	int index = 0;
-	int sign = 0;
-	char tempURL[256];
-	strcpy(tempURL, URL);
-	*wptLat=0;
-	*wptLon=0;
-
-	//search for lat= parameter
-	for (index=0; index < strlen(URL)-4; index++)
-	{	tempURL[index+4] = 0;
-		if (!strcmp(&tempURL[index], "lat=")) break;
-		else strcpy(tempURL, URL);
-	}
-	strcpy(tempURL, URL);
-	if (index >= strlen(URL)-4)
-	{	cout << "lat= parameter not found in URL!" << endl;
-		cout << "Skipping this point." << endl;
-		return 0;
-	}
-	index+=4; //seek past lat= parameter
-	//parse latitude
-	if (URL[index] == '-')
-	{	sign = -1;
-		index++;
-	}
-	else sign = 1;
-	while (URL[index] == '0' && index < strlen(URL)) index++;
-	if (URL[index] >= '1' && URL[index] <= '9')
-	{	*wptLat = (URL[index]-0x30);
-		index++;
-	}
-	while (URL[index] >= '0' && URL[index] <= '9' && index < strlen(URL))
-	{	*wptLat *= 10;
-		*wptLat += (URL[index]-0x30);
-		index++;
-	}
-	if (URL[index] == '.')
-	{	index++;
-		double DecDig=0;
-		while (URL[index] >= '0' && URL[index] <= '9' && index < strlen(URL))
-		{	DecDig--;
-			*wptLat += ((URL[index]-0x30)*pow(10,DecDig));
-			index++;
-		}
-	}
-	*wptLat *= sign;
-
-	//search for lon= parameter
-	for (index=0; index < strlen(URL)-4; index++)
-	{	tempURL[index+4] = 0;
-		if (!strcmp(&tempURL[index], "lon=")) break;
-		else strcpy(tempURL, URL);
-	}
-	strcpy(tempURL, URL);
-	if (index >= strlen(URL)-4)
-	{	cout << "lon= parameter not found in URL!" << endl;
-		cout << "Skipping this point." << endl;
-		return 0;
-	}
-	index+=4; //seek past lon= parameter
-	//parse longitude
-	if (URL[index] == '-')
-	{	sign = -1;
-		index++;
-	}
-	else sign = 1;
-	while (URL[index] == '0' && index < strlen(URL)) index++;
-	if (URL[index] >= '1' && URL[index] <= '9')
-	{	*wptLon = (URL[index]-0x30);
-		index++;
-	}
-	while (URL[index] >= '0' && URL[index] <= '9' && index < strlen(URL))
-	{	*wptLon *= 10;
-		*wptLon += (URL[index]-0x30);
-		index++;
-	}
-	if (URL[index] == '.')
-	{	index++;
-		double DecDig=0;
-		while (URL[index] >= '0' && URL[index] <= '9' && index < strlen(URL))
-		{	DecDig--;
-			*wptLon += ((URL[index]-0x30)*pow(10,DecDig));
-			index++;
-		}
-	}
-	*wptLon *= sign;
-
-	return 1;
-} //end parseURL
-
-highway* BuildRte
-(	char *filename,
-	char System[256],
-	char Region[256],
-	char Route[256],
-	char Banner[256],
-	char Abbrev[256],
-	char City[256],
-	char Root[256],
-	unsigned int NumAltRtes,
-	char **AltRouteNames,
-	char KeyValue[256]
-)
-
-{	// The WPT actually exists, right?
-	ifstream WPT1 (filename, ifstream::in);
-	if (!WPT1.is_open())
-	{	cout << filename << " file not found!\nSkipping this file." << endl;
-		return 0;
-	}
-	WPT1.seekg(0, ios::end);
-	if (!WPT1.tellg())
-	{	cout << filename << " filesize = 0!\nSkipping this file." << endl;
-		return 0;
-	}
-	WPT1.seekg(0);
-	ifstream WPT2 (filename, ifstream::in);
-
-	char str[4096];
-	int bookmark = 0;
-	highway *hwy = new highway(System, Region, Route, Banner, Abbrev, City, Root, NumAltRtes, AltRouteNames, KeyValue);
-	hwy->pt1 = new waypoint(0);
-	waypoint *point = hwy->pt1;
-
-	while (!WPT1.eof())
-	{	// read until URL found
-		while (WPT1 >> str && !strstr(str, "http://")) point->NumLabels += 1;
-		//cout << point->NumLabels << ' ';
-		point->InitLabels(); // located *here* to make ~waypoint() work.
-		//cout << WPT1.tellg() << ", ";
-
-		// exception for extra junk "labels" at end sans URL
-		if (WPT1.tellg() == -1) // reaches EOF without finding another URL
-			//FIXME some compilers will not like this conditional. Cause undefined behavior?
-		{	point = point->prev; //cout << "point = point->prev OK\n";
-			delete point->next;
-			point->next = 0;
-			//cout << "<BALEETED!> (break)\n";
-			break;
-		}
-		hwy->NumPts += 1;
-
-		// read in and store labels
-		WPT2.seekg(bookmark);
-		//cout << "seek " << bookmark << ", ";
-		for (unsigned int l = 0; l < point->NumLabels; l++)
-		{	WPT2 >> point->label[l]; // read in & store labels in their slots
-			// strip leading '+' from hidden & deprecated labels; leading '*' from closed point labels
-			while (point->label[l][0] == '+' || point->label[l][0] == '*')
-				strcpy (point->label[l], &point->label[l][1]);
-		}
-		WPT2 >> str; // read in the URL
-		bookmark = WPT1.tellg(); // bkmk the WPT file after the URL
-		//cout << "bkmk " << bookmark << ", \n";
-		// Mon 06 May 2013 01:32:31 PM EDT : Using WPT2 in addition to WPT1 FINALLY got around the missing CRLF bug!  w0ot w0ot
-
-		if (!parseURL(str, &point->OrigLat, &point->OrigLon))
-			if (point->prev) // if any point but the first failed
-			{	point = point->prev;
-				delete point->next;
-				if (!WPT1.eof())
-				{	point->next = new waypoint(point);
-					point = point->next;
-				}
-				else point->next = 0;
-			} // if the first point failed:
-			else { delete point; hwy->pt1 = new waypoint(0); point = hwy->pt1; }
-		else if (!WPT1.eof())
-		{	point->next = new waypoint(point);
-			point = point->next;
-		}
-	} //cout << "while (!WPT1.eof()) finished\n";
-	//cout << "\n\nbookmark = " << bookmark << " | tellg() = " << WPT1.tellg() << "\n\n";
-	WPT1.close(); WPT2.close(); //cout << "input files closed\n";
-
-	//cout << "BuildRte: " << filename << " | " << Root << " OK\n";
-	return hwy;
-} //end BuildRte
+#include "../lib/highway.cpp"	// includes cstring, fstream, iostream, list, string, vector
 
 inline void ColorCodes(string Color, char *UnColor, char *ClColor)
 //TODO input from INI file
@@ -352,18 +61,18 @@ void GetColors2(string SysCode, char *SysCSV, char *UnColor, char *ClColor)
 }
 
 void readlist(ofstream *html, highway *hwy)
-{	char Region[256], Name[256], pl1[256], pl2[256];
+{	string Region, Name, pl1, pl2;
 	unsigned int pi1, pi2;
 	ifstream list("list.list");
 	bool comma = 0;
 
 	while (list >> Region >> Name >> pl1 >> pl2) //FIXME: assumes perfectly formatted .list file with exactly four strings on every line
-		if (!strcmp(Region, hwy->Region) && hwy->NameMatch(Name))
-		{	while (pl1[0] == '+' || pl1[0] == '*') strcpy (pl1, &pl1[1]);
-			while (pl2[0] == '+' || pl2[0] == '*') strcpy (pl2, &pl2[1]);
-			pi1 = hwy->LblInd(pl1);
-			pi2 = hwy->LblInd(pl2);
-			if (pi1 < hwy->NumPts && pi2 < hwy->NumPts)
+		if (Region == hwy->Region && hwy->NameMatch(Name))
+		{	while (pl1[0] == '+' || pl1[0] == '*') pl1 = &pl1[1];
+			while (pl2[0] == '+' || pl2[0] == '*') pl2 = &pl2[1];
+			pi1 = hwy->GetIndByLabel(pl1);
+			pi2 = hwy->GetIndByLabel(pl2);
+			if (pi1 < hwy->pt.size() && pi2 < hwy->pt.size())
 			{	if (comma)
 					if (pi1 < pi2)	*html << ", " << pi1 << ", " << pi2;
 					else		*html << ", " << pi2 << ", " << pi1;
@@ -376,7 +85,7 @@ void readlist(ofstream *html, highway *hwy)
 	list.close();
 }
 
-void HTML(highway *hwy, char *SysCSV)
+void HTML(vector<highway*> hwy, char *SysCSV)
 {	char UnColor[6], ClColor[6];
 	char filename[] = "map.htm";
 	ofstream html(filename);
@@ -400,48 +109,38 @@ void HTML(highway *hwy, char *SysCSV)
 	html << "var rte = [];\n\n";
 
 	// route objects
-	for (unsigned int HwyNum = 0; hwy; hwy = hwy->next)
+	for (unsigned int num = 0; num < hwy.size(); num++)
 	{	// not all of this may be necessary but leaving it in nonetheless.
-		html << "rte[" << HwyNum << "] = {\n";
-		html << "System:\"" << hwy->System << "\", ";
-		html << "Region:\"" << hwy->Region << "\", ";
-		html << "Route:\"" << hwy->Route << "\", ";
-		html << "Banner:\"" << hwy->Banner << "\", ";
-		html << "Abbrev:\"" << hwy->Abbrev << "\", ";
-		html << "City:\"" << hwy->City << "\",\n";
+		html << "rte[" << num << "] = {\n";
+		html << "System:\"" << hwy[num]->System << "\", ";
+		html << "Region:\"" << hwy[num]->Region << "\", ";
+		html << "Route:\"" << hwy[num]->Route << "\", ";
+		html << "Banner:\"" << hwy[num]->Banner << "\", ";
+		html << "Abbrev:\"" << hwy[num]->Abbrev << "\", ";
+		html << "City:\"" << hwy[num]->City << "\",\n";
 
-		GetColors2(hwy->System, SysCSV, UnColor, ClColor);
+		GetColors2(hwy[num]->System, SysCSV, UnColor, ClColor);
 		html << "UnColor:'#" << UnColor << "', ClColor:'#" << ClColor << "',\n";
 
 		html << "//EDB - point latitudes\n";
 		html << "lat:[";
-		for (waypoint *point = hwy->pt1; point; point = point->next)
-		{	html << to_string(point->OrigLat);
-			if (point->next) html << ", ";
-		}
+		list<waypoint>::iterator point = hwy[num]->pt.begin();
+			html << to_string(point->OrigLat);
+		for (point++; point != hwy[num]->pt.end(); point++)
+			html << ", " << to_string(point->OrigLat);
 		html << "],\n";
 
 		html << "//EDB - point longitudes\n";
 		html << "lon:[";
-		for (waypoint *point = hwy->pt1; point; point = point->next)
-		{	html << to_string(point->OrigLon);
-			if (point->next) html << ", ";
-		}
+		point = hwy[num]->pt.begin();
+			html << to_string(point->OrigLon);
+		for (point++; point != hwy[num]->pt.end(); point++)
+			html << ", " << to_string(point->OrigLon);
 		html << "],\n";
 
-		/*html << "//vdeane - labels from highway browser\n";
-		html << "labels:[";
-		for (waypoint *point = hwy->pt1; point; point = point->next)
-		{	html << '"' << point->label[0] << '"';
-			if (point->next) html << ", ";
-		}
-		html << "],\n";//*/
-
 		html << "//vdeane & EDB - indices to .listfile endpoints\ncliSegments:[";
-		readlist(&html, hwy);
+		readlist(&html, hwy[num]);
 		html << "]\n} //end object definition\n\n";
-
-		HwyNum++;
 	} //end for (route objects)
 
 	html << "var MinLat = rte[rte.length-1].lat[0];\n";
@@ -512,83 +211,45 @@ void HTML(highway *hwy, char *SysCSV)
 	html << "</html>\n";
 }
 
-bool CSVmode(char** CharVars, unsigned int CharSkip, char *SysCSV)
-{	char System[256], Region[256], Route[256], Banner[256], Abbrev[256], City[256], Root[256], KeyValue[256];
-	char *InputFile = CharVars[3];
-	char *SourceDir = CharVars[6];
-	char CSVname[1024];
-	strcpy(CSVname, InputFile);
-	highway *FirstRte = 0;
-	highway *hwy = 0;
-	unsigned int i, bookmark, NumAltRtes;
-	char **AltRouteNames;
+bool CSVmode(string InputFile, string SourceDir, char* SysCSV)
+{	vector<highway*> HwyList;
 
-	ifstream CSV(CSVname, ios::in);
-	if (!CSV.is_open())
-	{	cout << CSVname << " file not found!" << endl;
+	ifstream CSV(InputFile.data());
+	if (!CSV)
+	{	cout << InputFile << " file not found!" << endl;
 		return 0;
 	}
+	CSV.seekg(0, ios::end); unsigned int EoF = CSV.tellg(); CSV.seekg(0);
+	while (CSV.get() != '\n' && CSV.tellg() < EoF); //skip header row
 
-	CSV.seekg(0);
-	while ((CSV.get() != '\n') && !CSV.eof()); //skip header row
-	while (!CSV.eof())
-	{	System[0] = CSV.get(); for (i = 0; i < 256 && System[i] != ';' && !CSV.eof(); System[i] = CSV.get()) i++; System[i] = 0;
-		Region[0] = CSV.get(); for (i = 0; i < 256 && Region[i] != ';' && !CSV.eof(); Region[i] = CSV.get()) i++; Region[i] = 0;
-		Route[0] = CSV.get(); for (i = 0; i < 256 && Route[i] != ';' && !CSV.eof(); Route[i] = CSV.get()) i++; Route[i] = 0; strcpy(KeyValue, &Route[CharSkip]);
-		Banner[0] = CSV.get(); for (i = 0; i < 256 && Banner[i] != ';' && !CSV.eof(); Banner[i] = CSV.get()) i++; Banner[i] = 0;
-		Abbrev[0] = CSV.get(); for (i = 0; i < 256 && Abbrev[i] != ';' && !CSV.eof(); Abbrev[i] = CSV.get()) i++; Abbrev[i] = 0;
-		City[0] = CSV.get(); for (i = 0; i < 256 && City[i] != ';' && !CSV.eof(); City[i] = CSV.get()) i++; City[i] = 0;
-		Root[0] = CSV.get(); for (i = 0; i < 256 && Root[i] != ';' && Root[i] != '\n' && !CSV.eof(); Root[i] = CSV.get()) i++;
-		char charlie = Root[i]; Root[i] = 0; NumAltRtes = 0;
-		if (charlie == ';') // AltRouteNames column exists
-		{	bookmark = CSV.tellg();
-			CSV.get(charlie);
-			if (charlie != '\n') NumAltRtes = 1;
-			while ((charlie != '\n') && !CSV.eof()) //FIXME tellg() filesize yadda yadda
-			{	if (charlie == ',') NumAltRtes++;
-				CSV.get(charlie);
-			}
-			AltRouteNames = new char*[NumAltRtes];
-			CSV.seekg(bookmark);
-			if (NumAltRtes)
-				for (unsigned int a = 0; a < NumAltRtes; a++)
-				{	AltRouteNames[a] = new char[256];
-					AltRouteNames[a][0] = CSV.get();
-					for (i = 0; i < 256 && AltRouteNames[a][i] != ',' && AltRouteNames[a][i] != '\n' && !CSV.eof(); AltRouteNames[a][i] = CSV.get())
-						i++;
-					AltRouteNames[a][i] = 0;
-				}
-			else CSV.get();
-		}
-		//construct input filename
-		strcpy(InputFile, SourceDir);
-		strcat(InputFile, System);
-		strcat(InputFile, "/");
-		strcat(InputFile, Root);
-		strcat(InputFile, ".wpt");
+	while (CSV.tellg() < EoF) // build hwy list
+	{	string System, Region, Route, Banner, Abbrev, City, Root, AltRouteNames;
+		string CSVline; // read individual line
+		for (char charlie = 0; charlie != '\n' && CSV.tellg() < EoF; CSVline.push_back(charlie)) CSV.get(charlie);
+		while (CSVline.back() == 0x0A || CSVline.back() == 0x0D)	// either DOS or UNIX...
+			CSVline.erase(CSVline.end()-1);				// strip out terminal '\n'
+		// parse CSV line
+		unsigned int i = 0;
+		while (CSVline[i] != ';') { System.push_back(CSVline[i]); i++; } i++;
+		while (CSVline[i] != ';') { Region.push_back(CSVline[i]); i++; } i++;
+		while (CSVline[i] != ';') { Route.push_back(CSVline[i]); i++; } i++;
+		while (CSVline[i] != ';') { Banner.push_back(CSVline[i]); i++; } i++;
+		while (CSVline[i] != ';') { Abbrev.push_back(CSVline[i]); i++; } i++;
+		while (CSVline[i] != ';') { City.push_back(CSVline[i]); i++; } i++;
+		while (CSVline[i] != ';' && i < CSVline.size()) { Root.push_back(CSVline[i]); i++; } i++;
+		while (CSVline[i] != ';' && i < CSVline.size()) { AltRouteNames.push_back(CSVline[i]); i++; } i++;
 
-		if (!CSV.eof())
-			if (!FirstRte)
-			{	FirstRte = BuildRte(InputFile, System, Region, Route, Banner, Abbrev, City, Root, NumAltRtes, AltRouteNames, KeyValue);
-				hwy = FirstRte;
-			} else
-			{	hwy->next = BuildRte(InputFile, System, Region, Route, Banner, Abbrev, City, Root, NumAltRtes, AltRouteNames, KeyValue);
-				if (hwy->next) hwy = hwy->next;
-			}
-	} //end while (step thru each line of CSV*/
-	cout << endl;
-	if (FirstRte)
-	{	HTML(FirstRte, SysCSV);
-		/*for (hwy = FirstRte; hwy; hwy = hwy->next)
-			hwy->CSVline();//*/
-	}
-} //end CSVmode
+		string wptFile = SourceDir+System+"/"+Root+".wpt";
+		highway *hwy = BuildRte(wptFile.data(), System, Region, Route, Banner, Abbrev, City, Root, AltRouteNames);
+		if (hwy) HwyList.push_back(hwy);
+	} // end while (build hwy list)
+
+	HTML(HwyList, SysCSV);
+}
 
 int main(int argc, char *argv[])
-{	char InputFile[1024] = "routes.csv";
-	char SourceDir[1024] = "data/";
-	char KeyValue[2] = "\0";
-	char* CharVars[7] = {0,0,0, InputFile, 0, KeyValue, SourceDir};
+{	string InputFile = "routes.csv";
+	string SourceDir = "data/";
 
 	if (argc != 2)
 	{	cout << "Usage: ./canvas SystemsCSVFile\n";
@@ -597,5 +258,5 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	CSVmode(CharVars, 0, argv[1]);
+	CSVmode(InputFile, SourceDir, argv[1]);
 }
