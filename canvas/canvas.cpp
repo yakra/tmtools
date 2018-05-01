f4cc80463bc4b494f78fd28506b42ac98fd6aff2
Eric Bryant <ericdbryant@yahoo.com>
2018-05-01 12:08:56 -0400

    DOS/UNIX \n & CSVline size fixes for systems.csv

--- canvas/canvas.cpp
+++ canvas/canvas.cpp
@@ -39,7 +39,8 @@ void GetColors2(string SysCode, char *SysCSV, char *UnColor, char *ClColor)
 	{	string System, CountryCode, Name, Color, Tier, Level;
 		string CSVline; // read individual line
 		for (char charlie = 0; charlie != '\n' && CSV.tellg() < EoF; CSVline.push_back(charlie)) CSV.get(charlie);
-		if (CSVline[CSVline.size()-1] == '\n') CSVline[CSVline.size()-1] = 0; //account for missing terminal '\n'
+		while (CSVline.back() == 0x0A || CSVline.back() == 0x0D)	// either DOS or UNIX...
+			CSVline.erase(CSVline.end()-1);				// strip out terminal '\n'
 		// parse CSV line
 		unsigned int i = 0;
 		while (CSVline[i] != ';') { System.push_back(CSVline[i]); i++; } i++;
