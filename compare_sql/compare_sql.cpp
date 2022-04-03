dcfb0c7c8d035fb6f9fca56ee1293e1018dbbf4d
eric bryant <ericdbryant@yahoo.com>
2022-04-03 16:49:53 -0400

    C++ sql file comparison tool

---
+++ compare_sql/compare_sql.cpp
@@ -0,0 +1,189 @@
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <thread>
+
+struct DB
+{	std::ifstream file;
+	std::list<std::string> lines;
+	std::string line;
+	int beg;
+
+	DB(const char* path): beg(1)
+	{	file.open(path);
+	}
+
+	void read_table()
+	{	beg += lines.size();
+		lines.clear();
+		if (line.size()) lines.emplace_back(std::move(line));
+		while (getline(file, line) && strncmp(line.data(), "CREATE TABLE", 12))
+		  lines.emplace_back(std::move(line));
+	}
+
+	std::string table_name()
+	{	if (!strncmp(lines.front().data(), "DROP TABLE IF EXISTS", 20)) return "DROP TABLE IF EXISTS";
+		if (!strncmp(lines.front().data(), "CREATE TABLE ", 13))
+		{	size_t space = lines.front().find(' ', 13);
+			if (space != -1) return lines.front().substr(13, space-13);
+		}
+		return "\x1B[31m" + lines.front() + "\x1B[0m";
+	}
+
+	int len()
+	{	return lines.size();
+	}
+
+	void sort()
+	{	lines.sort();
+	}
+
+	void xcommas()
+	{	for (auto& l : lines)
+		  if (l[0] == ',')
+		    l.erase(0,1);
+	}
+
+	void round(int p)
+	{	char fstr[39];
+		char pstr[12];
+		strcpy(pstr, "%.");
+		int e = sprintf(pstr+2, "%i", p);
+		strcpy(pstr+2+e, "g");
+		char* endptr;
+		for (auto& l : lines)
+		  for (size_t i = 0; i < l.size();)
+		  {	while (!isdigit(l[i]) && i < l.size()) ++i;
+			if (i < l.size())
+			{	double n = strtod(l.data()+i, &endptr);
+				e = sprintf(fstr, pstr, n);
+				l.replace(i, endptr-l.data()-i, fstr);
+				i += e;
+			}
+		  }
+	}
+
+	void xinsert()
+	{	std::string insert("INSERT INTO "+table_name()+" VALUES");
+		for (auto it = lines.begin(); it != lines.end();)
+		  if (*it == ";" || *it == insert)
+			it = lines.erase(it);
+		  else	it++;
+	}
+};
+
+void compare(DB& db1, DB& db2)
+{	if (db1.lines.size() != db2.lines.size())
+	{	std::cout << "\x1B[31mLine counts mismatch\x1B[0m " << db1.lines.size() << " <-> " << db2.lines.size() << std::endl;
+		return;
+	}
+	auto it1 = db1.lines.begin();
+	auto it2 = db2.lines.begin();
+	while (it1 != db1.lines.end() && it2 != db2.lines.end())
+	{	if (*it1 != *it2)
+		{	std::cout << "\x1B[31m" << *it1 << "\x1B[0m <-> \x1B[31m"  << *it2 << "\x1B[0m\n";
+			break;
+		}
+		++it1;
+		++it2;
+	}
+	if (it1 == db1.lines.end() && it2 == db2.lines.end())
+		std::cout << "\x1B[32mOK.\x1B[0m\n";
+}
+void sort   (DB& db1, DB& db2)
+{	std::thread thr1(&DB::sort, &db1);
+	std::thread thr2(&DB::sort, &db2);
+	thr1.join();	thr2.join();
+}
+void xcommas(DB& db1, DB& db2)
+{	std::thread thr1(&DB::xcommas, &db1);
+	std::thread thr2(&DB::xcommas, &db2);
+	thr1.join();	thr2.join();
+}
+void round  (DB& db1, DB& db2, int p)
+{	std::thread thr1(&DB::round, &db1, p);
+	std::thread thr2(&DB::round, &db2, p);
+	thr1.join();	thr2.join();
+}
+void xinsert(DB& db1, DB& db2)
+{	std::thread thr1(&DB::xinsert, &db1);
+	std::thread thr2(&DB::xinsert, &db2);
+	thr1.join();	thr2.join();
+}
+
+int main(int argc, char *argv[])
+{	if (argc != 3)
+	{	std::cout << "Please specify two versions to be compared.\n";
+		return 1;
+	}
+	DB db1(argv[1]); if (!db1.file.is_open()) { std::cout << "can't open " << argv[1] << std::endl; return 1; }
+	DB db2(argv[2]); if (!db2.file.is_open()) { std::cout << "can't open " << argv[2] << std::endl; return 1; }
+	std::cout << "    BEG1     LEN1                          TABLE     BEG2     LEN2\n";
+	std::cout << "-------- -------- ------------------------------ -------- --------\n";
+	while (!db1.file.eof() && !db2.file.eof())
+	{	std::thread thr1(&DB::read_table, &db1);
+		std::thread thr2(&DB::read_table, &db2);
+		thr1.join();
+		thr2.join();
+		std::string tablename(db1.table_name());
+		printf("%8i %8i %30s %8i %8i ", db1.beg, db1.len(), tablename.data(), db2.beg, db2.len()); fflush(stdout);
+
+		if (tablename == "clinched"				// 7332387
+		 || tablename == "regions"				// 512
+		   ) {	xcommas(db1, db2);
+			sort   (db1, db2);
+			compare(db1, db2);
+		     }	else
+		if (tablename == "waypoints"				// 1333538
+		 || tablename == "segments"				// 1246418
+		 || tablename == "datacheckErrors"			// 15588
+		 || tablename == "updates"				// 6153
+		 || tablename == "connectedRouteRoots"			// 3103
+		 || tablename == "systemUpdates"			// 716
+		 || tablename == "systems"				// 494
+		 || tablename == "countries"				// 211
+		 || tablename == "DROP TABLE IF EXISTS"			// 19
+		 || tablename == "continents"				// 10
+		   ) {	compare(db1, db2);
+		     }	else
+		if (tablename == "clinchedRoutes"			// 425822
+		 || tablename == "clinchedConnectedRoutes"		// 352890
+		   ) {	xinsert(db1, db2);
+			xcommas(db1, db2);
+			round  (db1, db2, 17);
+			sort   (db1, db2);
+			compare(db1, db2);
+		     }	else
+		if (tablename == "routes"				// 87121
+		 || tablename == "connectedRoutes"			// 84021
+		   ) {	round  (db1, db2, 17);
+			compare(db1, db2);
+		     }	else
+		if (tablename == "clinchedSystemMileageByRegion"	// 45013
+		 || tablename == "clinchedOverallMileageByRegion"	// 11459
+		   ) {	xcommas(db1, db2);
+			round  (db1, db2, 11);
+			sort   (db1, db2);
+			compare(db1, db2);
+		     }	else
+		if (tablename == "systemMileageByRegion"		// 1285
+		   ) {	round  (db1, db2, 12);	// Python
+			sort   (db1, db2);	// Python
+			compare(db1, db2);
+		     }	else
+		if (tablename == "graphs"				// 1227
+		 || tablename == "graphTypes"				// 11
+		   ) {	sort   (db1, db2);	// Python & C++
+			compare(db1, db2);	// in diff order
+		     }	else
+		if (tablename == "overallMileageByRegion"		// 344
+		   ) {	xcommas(db1, db2);	// Python
+			round  (db1, db2, 12);	// Python
+			sort   (db1, db2);	// Python
+			//round(db1, db2, 13);	// C++
+			compare(db1, db2);
+		     }
+		else std::cout << "\x1B[31m" << tablename << " not handled\x1B[0m" << std::endl;
+	}
+}
--- shell_scripts/compare_all.sh
+++ shell_scripts/compare_all.sh
@@ -74,9 +74,9 @@ echo '<<' `detect $v1` '<< >>' `detect $v2` '>>'
   rm -rf $v1/graphs.can; mkdir -p $v1/graphs.can
   rm -rf $v2/graphs.can; mkdir -p $v2/graphs.can
   echo "creating canonical $v1 graphs"
-  ../../canonicaltmg/canonicaltmg "$v1/graphs" "$v1/graphs.can"
+  ../../canonicaltmg/canonicaltmg "$v1/graphs" "$v1/graphs.can" || exit 1
   echo "creating canonical $v2 graphs"
-  ../../canonicaltmg/canonicaltmg "$v2/graphs" "$v2/graphs.can"
+  ../../canonicaltmg/canonicaltmg "$v2/graphs" "$v2/graphs.can" || exit 1
   # diff
   echo -e "comparing graphs                                 \e[31m"
   diff -q -r $v1/graphs.can/ $v2/graphs.can/
@@ -89,97 +89,5 @@ echo '<<' `detect $v1` '<< >>' `detect $v2` '>>'
   echo -en "\e[0m"
 
 # do sql files
-  std () {
-    echo -e "\e[31m$(diff -q	<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1) \
-				<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2) )\e[0m"
-  }
-  sorted () {
-    echo -en "\e[2mSorting... \e[0m"
-    echo -e "\e[31m$(diff -q	<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1 | sort) \
-				<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2 | sort) )\e[0m"
-  }
-  trunc () {
-    dig=$1
-    echo -en "\e[2mTruncating to $dig decimal places... \e[0m"
-    echo -e "\e[31m$(diff -q	<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1 | sed -r "s~(\.[0-9]{$dig})[0-9]*~\1~") \
-				<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2 | sed -r "s~(\.[0-9]{$dig})[0-9]*~\1~") )\e[0m"
-  }
-  pointzero () {
-    echo -en "\e[2mIgnoring .0 on integers... \e[0m"
-    echo -e "\e[31m$(diff -q	<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1 | sed "s~\.0'~'~") \
-				<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2 | sed "s~\.0'~'~") )\e[0m"
-  }
-  multi () {
-    dig=$1
-    echo -en "\e[2mmulti($dig)... \e[0m"
-    echo -e "\e[31m$(diff -q	<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1 | sed -r -e 's~^,~~' -e "s~\.0'~'~" -e "s~(\.[0-9]{$dig})[0-9]*~\1~g" | sort) \
-				<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2 | sed -r -e 's~^,~~' -e "s~\.0'~'~" -e "s~(\.[0-9]{$dig})[0-9]*~\1~g" | sort) )\e[0m"
-  }
-  insert () {
-    dig=$1
-    echo -en "\e[2minsert($dig)... \e[0m"
-    c=$(diff -q	<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1 | grep -v "^INSERT INTO $table1 VALUES$\|^;$" | sed -r -e 's~^,~~' -e "s~\.0'~'~" -e "s~(\.[0-9]{$dig})[0-9]*~\1~g" | sort) \
-		<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2 | grep -v "^INSERT INTO $table2 VALUES$\|^;$" | sed -r -e 's~^,~~' -e "s~\.0'~'~" -e "s~(\.[0-9]{$dig})[0-9]*~\1~g" | sort) )
-    echo -e "\e[31m$c\e[0m"
-    if [ "$c" != '' ]; then
-      echo -e "\n\e[1;4mTry this incantation:\e[0m"
-      echo "diff \\"
-      echo "<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1 | grep -v \"^INSERT INTO $table1 VALUES$\|^;$\" | sed -r -e 's~^,~~' -e \"s~\.0'~'~\" -e \"s~(\.[0-9]{$dig})[0-9]*~\1~g\" | sort) \\"
-      echo "<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2 | grep -v \"^INSERT INTO $table2 VALUES$\|^;$\" | sed -r -e 's~^,~~' -e \"s~\.0'~'~\" -e \"s~(\.[0-9]{$dig})[0-9]*~\1~g\" | sort) \\"
-      echo -e "| less -r\n"
-    fi
-  }
-  nocommas () {
-    echo -en "\e[2mSorting and ignoring commas... \e[0m"
-    echo -e "\e[31m$(diff -q	<(tail -n +$beg1 $v1/TravelMapping.sql | head -n $len1 | sed 's~^,~~' | sort) \
-				<(tail -n +$beg2 $v2/TravelMapping.sql | head -n $len2 | sed 's~^,~~' | sort) )\e[0m"
-  }
   echo -e "comparing .sql files"
-  nums1=`grep -n 'CREATE TABLE' $v1/TravelMapping.sql | cut -f1 -d:`
-  nums2=`grep -n 'CREATE TABLE' $v2/TravelMapping.sql | cut -f1 -d:`
-  wc1=$(expr `cat $v1/TravelMapping.sql | wc -l` + 1)
-  wc2=$(expr `cat $v2/TravelMapping.sql | wc -l` + 1)
-
-  echo "    BEG1     LEN1                          TABLE     BEG2     LEN2"
-  echo "-------- -------- ------------------------------ -------- --------"
-  for item in $(paste -d- <(echo -e "$nums1\n$wc1") <(echo -e "1\n$nums1") <(echo -e "$nums2\n$wc2") <(echo -e "1\n$nums2")); do
-    beg1=`echo $item | cut -f2 -d-`
-    beg2=`echo $item | cut -f4 -d-`
-    len1=`echo $item | sed -r 's~-[0-9]+-[0-9]+$~~' | bc`
-    len2=`echo $item | sed -r 's~^[0-9]+-[0-9]+-~~' | bc`
-    table1=$(tail -n +$beg1 $v1/TravelMapping.sql | head -n 1 | sed -e 's~ (.*~~' -e 's~CREATE TABLE ~~' -e 's~\(DROP TABLE IF EXISTS\).*~\1...~')
-    table2=$(tail -n +$beg2 $v2/TravelMapping.sql | head -n 1 | sed -e 's~ (.*~~' -e 's~CREATE TABLE ~~' -e 's~\(DROP TABLE IF EXISTS\).*~\1...~')
-    if [ "$table1" != "$table2" ]; then
-      echo -e "\e[31m< $beg1 $len1 $table1"
-      echo -e "> $beg2 $len2 $table2\e[0m"
-      exit 1
-    fi
-
-    printf "%8i %8i %30s %8i %8i " $beg1 $len1 "$table1" $beg2 $len2
-
-    case "$table1" in
-      'DROP TABLE IF EXISTS...') std;;
-      'continents')			std;;
-      'countries')			std;;
-      'regions')			nocommas;;
-      'systems')			std;;
-      'routes')				trunc 10;;
-      'connectedRoutes')		trunc 7;;
-      'connectedRouteRoots')		std;;
-      'waypoints')			pointzero;;
-      'segments')			std;;
-      'clinched')			nocommas;;
-      'overallMileageByRegion')		multi 8;;
-      'systemMileageByRegion')		multi 8;;
-      'clinchedOverallMileageByRegion')	multi 7;;
-      'clinchedSystemMileageByRegion')	multi 7;;
-      'clinchedConnectedRoutes')	insert 7;;
-      'clinchedRoutes')			insert 7;;
-      'updates')			std;;
-      'systemUpdates')			std;;
-      'datacheckErrors')		std;;
-      'graphTypes')			sorted;;
-      'graphs')				sorted;;
-        *) echo -en "\e[2munsupported; attempting std... \e[0m"; std;;
-    esac
-  done
+  ../../../tmtools/compare_sql/compare_sql $v1/TravelMapping.sql $v2/TravelMapping.sql
