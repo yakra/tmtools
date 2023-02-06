cdbb98562c78b95d6215b128562fa8256d94ad00
eric bryant <ericdbryant@yahoo.com>
2023-02-05 21:15:45 -0500

    write terminal LF iff original file has it
    and preserve blank lines at end of file
    (Whitespace-only lines? I'll cross that bridge when I come to it!)

--- shell_scripts/unusedaltlabels.sh
+++ shell_scripts/unusedaltlabels.sh
@@ -11,7 +11,16 @@ for ualentry in `$op "$ualfile" | grep "^$rg\." | tr ' ' '%'`; do
   file=*/`echo "$ualentry" | cut -f1 -d'('`.wpt
   uals=`echo "$ualentry" | cut -f2 -d: | tr % '\n'`
   echo $file
+
+  num_lines=`cat $file | wc -l`
+  if [ "$(head -n $num_lines $file)" == "$(cat $file)" ]
+    then term_crlf=1
+    else term_crlf=0; num_lines=`expr $num_lines + 1`
+  fi
+
+  line_num=0
   for line in `cat $file | tr ' ' '%'`; do
+    line_num=`expr $line_num + 1`
     label=`echo "$line" | sed 's~%.*~~'`
     url=`echo "$line" | sed 's~.*%~~'`
     echo -n "$label" >> unusedaltlabels.tmp
@@ -19,7 +28,12 @@ for ualentry in `$op "$ualfile" | grep "^$rg\." | tr ' ' '%'`; do
       trim=`echo $alt | tr -d +`
       if [ `echo "$uals" | grep -i "^+*$trim$" | wc -l` == 0 ]; then echo -n " $alt" >> unusedaltlabels.tmp; fi
     done
-    echo " $url" >> unusedaltlabels.tmp
+    echo -n " $url" >> unusedaltlabels.tmp
+    if [ $line_num != $num_lines ] || [ $term_crlf == 1 ]; then echo >> unusedaltlabels.tmp; fi
+  done
+  while [ $line_num -lt $num_lines ]; do
+    echo >> unusedaltlabels.tmp
+    line_num=`expr $line_num + 1`
   done
   mv unusedaltlabels.tmp $file
 done
